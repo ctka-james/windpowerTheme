c5f8a450109a3bc0104e0a3b73152e54d534e424
JamesOffice <james@u-sen.com.tw>
2022-06-24 17:22:43 +0800

    wordpress theme dev

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <title><?php bloginfo('name'); ?></title>
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="well well-md">
+            <h1><?php bloginfo('name'); ?></h1>
+            <em><?php bloginfo('description'); ?></em>
+        </div>
+    </div>
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php get_header(); ?>
+<div>
+    <div>
+        <ul>
+            <?php
+            global $query_string;
+            $arg = array(
+                'category_in' => array(2, 6),
+                'posts_per_page' => 4,
+                'order' => 'ASC',
+            );
+            query_posts($arg);
+            // query_posts($query_string . '&posts_per_page=5&order=ASC');
+
+            if (have_posts()) :
+                while (have_posts()) :
+                    the_post();
+            ?>
+                    <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+            <?php
+                endwhile;
+            endif;
+            ?>
+        </ul>
+    </div>
+</div>
+
+<?php get_footer(); ?>
